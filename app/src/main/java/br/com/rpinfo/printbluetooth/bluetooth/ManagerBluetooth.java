770e3d5d5edf92e96ce3c0753fb4ae962bc0a73d
Juliano Sirtori <julianosirtori@gmail.com>
2017-11-23 18:09:07 -0200

    Criado controller para mandar impresoes

--- app/src/main/java/br/com/rpinfo/printbluetooth/bluetooth/ManagerBluetooth.java
+++ app/src/main/java/br/com/rpinfo/printbluetooth/bluetooth/ManagerBluetooth.java
@@ -7,8 +7,6 @@ import android.content.Context;
 import android.content.Intent;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -24,12 +22,6 @@ public class ManagerBluetooth {
     public final static int CODE_ACTIVE_BLUETOOTH = 1;
     //uuid da impressora bluetooth
     public static final UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-
-    public BluetoothDevice device;
-    private BluetoothSocket socket;
-    private InputStream in;
-    private OutputStream out;
-
 
     private Context context;
     private BluetoothAdapter btfAdapter;
---
+++ app/src/main/java/br/com/rpinfo/printbluetooth/bluetooth/PrintController.java
@@ -0,0 +1,51 @@
+package br.com.rpinfo.printbluetooth.bluetooth;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Vector;
+
+/**
+ * Criado por Juliano Sirtori em 23/11/2017.
+ */
+
+public class PrintController {
+
+    public BluetoothDevice device;
+    private BluetoothSocket socket;
+    private InputStream in;
+    private OutputStream out;
+
+    private boolean running;
+
+    public PrintController(BluetoothSocket socket) throws IOException {
+        this.socket = socket;
+        this.in = socket.getInputStream();
+        this.out = socket.getOutputStream();
+        this.running = true;
+    }
+
+    public boolean sendPrint(byte[] bytes) throws IOException {
+        if (out != null) {
+            out.write(bytes);
+            return true;
+        }
+        return false;
+    }
+
+    public void stop() throws IOException {
+        running = false;
+        if (socket != null) {
+            socket.close();
+        }
+        if (in != null) {
+            in.close();
+        }
+        if (out != null) {
+            out.close();
+        }
+    }
+}
